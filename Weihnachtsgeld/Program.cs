6a7c62355ae222e8c32124e9f4537a964bc72f07
Matthias Jung <maddesjg@hotmail.de>
2019-05-10 07:57:39 +0200

    Add project files.

---
+++ Weihnachtsgeld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weihnachtsgeld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Namen Eingeben: ");
+            Console.Write("Alter Eingeben:");
+            Console.Write("Wie Lange im Unternehmen:");
+            Console.Write("");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.Write("\ntext");
+            Console.ReadKey();
+
+        }
+    }
+}
